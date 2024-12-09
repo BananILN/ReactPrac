4c4d3f79a797402390ba3bec022dd0cd9a08832e
BananILN <daniil.klovans84@mail.ru>
2024-12-10 00:24:17 +0400

    Add hide components

--- React-prac/src/App.jsx
+++ React-prac/src/App.jsx
@@ -3,11 +3,12 @@ import AppCharacters from './components/AppCharacrets'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
      <div>HI</div>
+     
      <AppCharacters />
     </>
   )
--- React-prac/src/components/AppCharacrets.jsx
+++ React-prac/src/components/AppCharacrets.jsx
@@ -1,29 +1,74 @@
-import { useState } from "react";
-import "./styles.css"
-import { getRandomHexColor, getRandomElementOfArray } from "./utils"
-import {API_URL} from "./api"
+import { useEffect, useRef, useState } from "react";
+import "./styles.css";
+import { getRandomHexColor, getRandomElementOfArray } from "./utils";
+import { API_URL } from "./api";
 
-export default function AppCharacters(){
-    const [hero, setHero] = useState({name:'undefined'})
-    const randColor = getRandomHexColor();
-    
-    const getRandomHeroHandler = () =>{
-        fetch(API_URL)
-        .then((response) => response.json())
-        .then((data)=>{
-            const hero = data.results;
-            const randomHero = getRandomElementOfArray(hero);
-            setHero({name: randomHero.name})
-        })
+
+export default function Character  ()  {
+    const [visibility, setVisibility] = useState(true);
+
+    if (!visibility) {
+        return null;
     }
-    
-    
+
+ 
+
+    return (
+        <AppCharacters setVisibility={setVisibility} />
+    );
+}
+
+
+
+ function AppCharacters({ setVisibility }) {
+    const intervalRef = useRef(undefined);
+    const [hero, setHero] = useState({ name: 'undefined' });
+    const randColor = getRandomHexColor();
+
+
+    useEffect(() => {
+        console.log("Component Mount");
+        getRandomHero();
+
+        intervalRef.current = setInterval(() => {
+            getRandomHero();
+        }, 3000);
+
+        return () => {
+            console.log("Component: UnMount");
+            clearInterval(intervalRef.current);
+        };
+    }, []);
+
+    useEffect(() => {
+        console.log("Component: Render");
+    });
+
+    const getRandomHero = () => {
+        fetch(API_URL)
+            .then((response) => response.json())
+            .then((data) => {
+                const hero = data.results;
+                const randomHero = getRandomElementOfArray(hero);
+                setHero({ name: randomHero.name });
+            });
+    }
+
+    const getRandomHeroHandler = () => {
+        getRandomHero();
+    }
+
+    const hideComponentHandler = () => {
+        setVisibility(false);
+    }
+
     return (
         <>
-            <h2 className="Character" style={{ backgroundColor: randColor}}>
+            <button onClick={hideComponentHandler}> Hide component</button>
+            <h2 className="Character" style={{ backgroundColor: randColor }}>
                 Character: {hero.name}
             </h2>
             <button onClick={getRandomHeroHandler}> Get Random Hero</button>
         </>
-    )
+    );
 }
